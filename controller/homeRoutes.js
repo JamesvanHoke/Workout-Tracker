6f4f13fca886c1950220216152b3664f0e8e7d63
James V <james.vanhoke@yahoo.com>
2021-04-17 01:32:09 -0700

    Homeroutes

--- controller/homeRoutes.js
+++ controller/homeRoutes.js
@@ -2,7 +2,18 @@ const router = require("express").Router();
 const path = require("path");
 
 // get /
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
 // get /exercise
+router.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
 
 // get /stats
+router.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+module.exports = router
